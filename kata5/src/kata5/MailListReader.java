3b7e31035a354c2706a968bbc5ba7f84f5452053
Edgar Holzmann <e.holzmann.bvb@googlemail.com>
2019-11-27 10:12:45 +0000

    Create the class Mail and Load the emails of the archieve email.txt into a list.

--- kata5/src/kata5/MailListReader.java
+++ kata5/src/kata5/MailListReader.java
@@ -1,5 +1,32 @@
 package kata5;
 
-public class MailListReader {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class MailListReader {
+    public static List<String> read(String fileName) {
+        List<String> list = new ArrayList<>();
+        
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
+            while(true){
+                String line = reader.readLine();
+                if(line == null) break;
+                if(Mail.isMail(line)) list.add(new String(line));
+            }
+        }
+        catch(FileNotFoundException exeption){
+            System.out.println("ERROR: MailListReader::read(FileNotFoundException)");
+        }
+        catch(IOException exeption){
+            System.out.println("ERROR: MailListReader::read(IOException)");
+        }
+        
+        return list;
+    }
 }

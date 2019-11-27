1af180bf52e6d8e602a437ef9d8a68cba49de675
Edgar Holzmann <e.holzmann.bvb@googlemail.com>
2019-11-27 09:32:32 +0000

    Realize the query of the table PEOPLE.

--- kata5/src/kata5/Kata5.java
+++ kata5/src/kata5/Kata5.java
@@ -2,12 +2,14 @@ package kata5;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Kata5 {
 
     public static void main(String[] args) {
-        String URL_BD = new String("jdbc:sqlite:C:\\Users\\Usuario\\Documents\\NetBeansProjects\\kata5");
+        String URL_BD = new String("jdbc:sqlite:C:\\Users\\Usuario\\Documents\\NetBeansProjects\\kata5\\kata5.db");
         Connection connection = connect(URL_BD);
     }
 
@@ -16,20 +18,34 @@ public class Kata5 {
         try{
             connection = DriverManager.getConnection(URL_BD);
             System.out.println("Se ha conectado a la base de datos con exito.");
-            
+            ShowTableData(connection);
         }catch(SQLException ex){
-            System.out.print("ERROR kata5 " + ex.getMessage());
+            System.out.println("ERROR kata5 " + ex.getMessage());
         }
         finally{
             try{
                 if(connection != null) connection.close();
             }catch(SQLException ex){
-                System.out.print("ERROR kata5 " + ex.getMessage());   
+                System.out.println("ERROR kata5 " + ex.getMessage());   
             }
         }
         return connection;
         }
-        
-    
-    
+
+    private static void ShowTableData(Connection connection) {
+        String SQL_Sentence = "SELECT * FROM PEOPLE";
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet resultset = statement.executeQuery(SQL_Sentence);
+            System.out.println("Id \t Name \t Apellidos \t Departamento");
+            while(resultset.next()){
+                System.out.println(resultset.getInt("Id") + " \t " +
+                        resultset.getString("Name") + " \t " +
+                        resultset.getString("Apellidos") + " \t " +
+                        resultset.getString("Departamento"));
+            }
+        }catch(SQLException ex){
+            System.out.print("ERROR kata5 " + ex.getMessage());
+        }
+    }
 }
